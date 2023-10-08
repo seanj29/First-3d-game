7ebca4033010493d9cd89e2dfec31a0022e77d34
seanj29 <seanotto29@gmail.com>
2023-10-08 19:54:37 +0100

    Added player and changed project

---
+++ Player.gd
@@ -0,0 +1,21 @@
+extends KinematicBody
+
+export var speed = 14
+
+export var fall_acceleration = 75
+
+var velocity = Vector3.ZERO
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
