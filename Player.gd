d25f06e60bfbcffe1398527ef91285aec95abc1a
seanj29 <seanotto29@gmail.com>
2023-10-08 20:45:38 +0100

    updated main

--- Player.gd
+++ Player.gd
@@ -6,16 +6,31 @@ export var fall_acceleration = 75
 
 var velocity = Vector3.ZERO
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+	
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.look_at(translation + direction, Vector3.UP)
+		
+	#Ground Velocity
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	#Vertical Velocity
+	velocity.y -= fall_acceleration * delta
+	#Moving the character
+	velocity = move_and_slide(velocity, Vector3.UP)
